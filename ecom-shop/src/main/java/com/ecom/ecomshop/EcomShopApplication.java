e8b2daff6126c83e06a6baf98bde77042d5357b5
Jayklef <arhawhojerry@gmail.com>
2026-05-26 01:25:54 +0100

    first commit

---
+++ ecom-shop/src/main/java/com/ecom/ecomshop/EcomShopApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.ecomshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomShopApplication.class, args);
+    }
+
+}
