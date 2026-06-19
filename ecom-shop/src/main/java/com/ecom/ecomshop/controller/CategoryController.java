93eaf0d359e51fa1a7b1ced1a35ae1154ade7128
Jayklef <arhawhojerry@gmail.com>
2026-06-19 16:06:36 +0100

    ResourceNotFoundException and APIException

--- ecom-shop/src/main/java/com/ecom/ecomshop/controller/CategoryController.java
+++ ecom-shop/src/main/java/com/ecom/ecomshop/controller/CategoryController.java
@@ -6,9 +6,8 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 @RestController
@@ -35,24 +34,16 @@ public class CategoryController {
 
 
     @PutMapping("/admin/categories/{categoryId}")
-    public ResponseEntity<String> updateCategory(@PathVariable Long categoryId,
+    public ResponseEntity<String> updateCategory(@Valid @PathVariable Long categoryId,
                                                   @RequestBody Category category){
-        try {
             Category savedCategory = categoryService.updateCategory(categoryId, category);
             return new ResponseEntity<>("Category with id: " + categoryId + "updated successfully", HttpStatus.OK);
-        } catch (ResponseStatusException e){
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
     }
 
     @DeleteMapping("/admin/categories/{categoryId}")
     public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId){
-        try {
             String status = categoryService.deleteCategory(categoryId);
             return new ResponseEntity<>(status, HttpStatus.OK);
-        } catch (ResponseStatusException e){
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
 
     }
 }
