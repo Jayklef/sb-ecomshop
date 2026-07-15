638036d4cff4350af8e34befd4bb9098f3430fa1
Jayklef <arhawhojerry@gmail.com>
2026-07-15 08:49:10 +0100

    Implemented sorting and APIResponse for Exception

--- ecom-shop/src/main/java/com/ecom/ecomshop/controller/CategoryController.java
+++ ecom-shop/src/main/java/com/ecom/ecomshop/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package com.ecom.ecomshop.controller;
 
+import com.ecom.ecomshop.config.AppConstants;
 import com.ecom.ecomshop.model.Category;
 import com.ecom.ecomshop.payload.CategoryDTO;
 import com.ecom.ecomshop.payload.CategoryResponse;
@@ -22,9 +23,11 @@ public class CategoryController {
 
     @GetMapping("/public/categories")
     public ResponseEntity<CategoryResponse> getAllCategories(
-            @RequestParam(name = "pageNumber") Integer pageNumber,
-            @RequestParam(name = "pageSize") Integer pageSize){
-       CategoryResponse categoryResponse = categoryService.getAllCategories(pageNumber, pageSize);
+            @RequestParam(name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+            @RequestParam(name = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+            @RequestParam(name = "sortBy", defaultValue = AppConstants.SORT_CATEGORIES_BY, required = false) String sortBy,
+            @RequestParam(name = "sortOrder", defaultValue = AppConstants.SORT_DIR, required = false) String sortOrder){
+       CategoryResponse categoryResponse = categoryService.getAllCategories(pageNumber, pageSize, sortBy, sortOrder);
         return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
     }
 
