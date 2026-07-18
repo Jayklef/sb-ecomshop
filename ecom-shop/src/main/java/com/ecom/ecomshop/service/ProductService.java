f5fe32b57ec4f3715f4730ae879ebd259e5e8336
Jayklef <arhawhojerry@gmail.com>
2026-07-18 18:22:33 +0100

    ProductDTO and ProductResponse implementation

--- ecom-shop/src/main/java/com/ecom/ecomshop/service/ProductService.java
+++ ecom-shop/src/main/java/com/ecom/ecomshop/service/ProductService.java
@@ -1,4 +1,12 @@
 package com.ecom.ecomshop.service;
 
+import com.ecom.ecomshop.model.Product;
+import com.ecom.ecomshop.payload.ProductDTO;
+import com.ecom.ecomshop.payload.ProductResponse;
+
 public interface ProductService {
+
+    ProductDTO addProduct(Long categoryId, Product product);
+
+    ProductResponse getAllProducts();
 }
--- ecom-shop/src/main/java/com/ecom/ecomshop/service/ProductServiceImpl.java
+++ ecom-shop/src/main/java/com/ecom/ecomshop/service/ProductServiceImpl.java
@@ -1,7 +1,56 @@
 package com.ecom.ecomshop.service;
 
+import com.ecom.ecomshop.exceptions.ResourceNotFoundException;
+import com.ecom.ecomshop.model.Category;
+import com.ecom.ecomshop.model.Product;
+import com.ecom.ecomshop.payload.ProductDTO;
+import com.ecom.ecomshop.payload.ProductResponse;
+import com.ecom.ecomshop.repositories.CategoryRepository;
+import com.ecom.ecomshop.repositories.ProductRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.security.PrivateKey;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductServiceImpl implements ProductService{
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public ProductDTO addProduct(Long categoryId, Product product){
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(()-> new ResourceNotFoundException("Category", "categoryId", categoryId));
+
+        product.setImage("default png");
+        product.setCategory(category);
+        double specialPrice = product.getPrice() -
+                ((product.getDiscount() * 0.01) * product.getPrice());
+
+        product.setSpecialPrice(specialPrice);
+        Product savedProduct = productRepository.save(product);
+        return modelMapper.map(savedProduct, ProductDTO.class);
+    }
+
+    @Override
+    public ProductResponse getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        List<ProductDTO> productDTOS = products.stream()
+                .map(product -> modelMapper.map(product, ProductDTO.class))
+                .toList();
+
+        ProductResponse productResponse = new ProductResponse();
+        productResponse.setContent(productDTOS);
+        return productResponse;
+    }
 }
