052de7e3c996534566cf6d0ba68480c70466f8fe
Jayklef <arhawhojerry@gmail.com>
2026-07-18 15:39:40 +0100

    Product, product service, repo, Impl, dto, prodresponse

---
+++ ecom-shop/src/main/java/com/ecom/ecomshop/service/ProductService.java
@@ -0,0 +1,4 @@
+package com.ecom.ecomshop.service;
+
+public interface ProductService {
+}
---
+++ ecom-shop/src/main/java/com/ecom/ecomshop/service/ProductServiceImpl.java
@@ -0,0 +1,7 @@
+package com.ecom.ecomshop.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+}
