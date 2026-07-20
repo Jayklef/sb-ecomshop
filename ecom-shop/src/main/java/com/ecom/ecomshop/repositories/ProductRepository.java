f2f94c33c61754c7a94d9f66e0c201a9c58f922e
Jayklef <arhawhojerry@gmail.com>
2026-07-20 17:55:30 +0100

    product by category implementation

--- ecom-shop/src/main/java/com/ecom/ecomshop/repositories/ProductRepository.java
+++ ecom-shop/src/main/java/com/ecom/ecomshop/repositories/ProductRepository.java
@@ -1,9 +1,13 @@
 package com.ecom.ecomshop.repositories;
 
+import com.ecom.ecomshop.model.Category;
 import com.ecom.ecomshop.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByCategoryOrderByPriceAsc(Category category);
 }
